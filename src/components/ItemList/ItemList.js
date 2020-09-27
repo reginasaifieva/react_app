700c2438e8f79fe181212b38cee44ec5cc110fe7
Regina <saifieva98@gmail.com>
2020-09-27 23:09:22 +0300

    create structure

---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Item from '../Item/Item'
+const ItemList = () => (
+	<ul>
+		<li><Item/></li>
+		<li><Item/></li>
+		<li><Item/></li>
+	</ul>
+	);
+
+export default ItemList;
