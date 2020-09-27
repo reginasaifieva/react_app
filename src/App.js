29476c9a22cb69947ecf17b664101350e5a1c8f2
Regina <saifieva98@gmail.com>
2020-09-27 22:07:08 +0300

    create react-component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
 import React from 'react';
-const element = (<div>Hello World!</div>);
-export {element};
+const ItemList = () => (<ul>
+<li>1</li>
+<li>2</li>
+  </ul>);
+const App = () => (<div>
+<h1>Hello World!</h1>
+<ItemList/>
+</div>);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {element} from './App';
+import App from './App';
 
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
