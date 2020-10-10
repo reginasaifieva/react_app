cb98ddedaff564e28576b07e5cdc360891f048db
Regina <saifieva98@gmail.com>
2020-10-10 14:18:49 +0300

    use css modules

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
-import './App.css';
+import styles from './App.module.css';
 
 const App = () => {
 	const items = [
@@ -17,8 +17,8 @@ const App = () => {
 	];
 
 	return (
-		<div className='wrap'>
-					<h1 className="wrap__title">Важные дела:</h1>
+		<div className={styles.wrap}>
+					<h1 className={styles.title}>Важные дела:</h1>
 					<ItemList items={items}/>
 					<Footer count={6}/>
 	</div>);
