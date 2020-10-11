ec0947efe4c9b291910ffe6a732dce2575060843
Regina <saifieva98@gmail.com>
2020-10-11 14:22:29 +0300

    change app in class-component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,8 +4,9 @@ import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const App = () => {
-	const items = [
+class App extends React.Component {
+	render () {
+		const items = [
 		{
 			value: 'Написать новое приложение',
 			isDone: true
@@ -19,14 +20,15 @@ const App = () => {
 		isDone: false
 		}
 	];
-
-	return (
-		<div className={styles.wrap}>
-					<h1 className={styles.title}>Важные дела:</h1>
-					<InputItem/>
-					<ItemList items={items}/>
-					<Footer count={6}/>
-	</div>);
+		return (
+					<div className={styles.wrap}>
+						<h1 className={styles.title}>Важные дела:</h1>
+						<InputItem/>
+						<ItemList items={items}/>
+						<Footer count={6}/>
+				</div>
+				)
+		}
 }
 
 export default App;
