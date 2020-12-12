0c771280ff3c46c6272b6ea39c0d929e22877332
Regina <saifieva98@gmail.com>
2020-12-12 15:22:50 +0300

    add task

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,59 +1,38 @@
 import React from 'react';
-import styles from './item.module.css';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { withStyles } from '@material-ui/core/styles';
+import styles from './item.module.css';
+import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-
-const stl = {
-  done: {
-    textDecoration: 'line-through'
-  }
-}
 class Item extends React.Component {
-  componentDidMount() {
-    this.timerID = setInterval(() => console.log('interval'), 1000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
   render() {
-    const {value, isDone, classes, onClickDone, id, onClickDelete} = this.props;
-    return (
-        <ListItem fullWidth>
-            <Checkbox
-                checked={isDone}
-                tabIndex={-1}
-                onClick={() => onClickDone(id)}
-            />
-            <ListItemText primary={value} classes={{
-                root: isDone && classes.done 
-            }} />
-            <ListItemSecondaryAction>
-                <IconButton aria-label="Delete" 
-                className={styles.list__delete}>
-                    <DeleteIcon onClick={() => onClickDelete(id)}/>
-                </IconButton>
-            </ListItemSecondaryAction>
-        </ListItem>);
-  } 
-}
+    const { value, isDone, id, onClickDone, onClickDelete } = this.props;
 
-
-Checkbox.defaultProps = {
-  isDone: false
-};
-Item.propTypes = {
-  isDone:  PropTypes.bool,
-  value: PropTypes.string.isRequired,
-  onClickDone: PropTypes.func.isRequired,
-  onClickDelete: PropTypes.func.isRequired,
-  id: PropTypes.number.isRequired
+    return (<div className={styles.wrap}>
+      <Checkbox
+        color="default"
+        checked={isDone}
+        onClick={() => onClickDone(id)}
+      />
+      <div className={
+        classnames({
+          [styles.item]: true,
+          [styles.done]: isDone
+        })}> 
+      {value}
+      </div>
+      <IconButton
+        className={styles.button}
+        aria-label="delete"
+      >
+        <DeleteIcon
+          onClick={() => onClickDelete(id)}
+        />
+      </IconButton>
+    </div>)
+  }
 };
 
-export default withStyles(stl)(Item);
+export default Item;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import Item from '../Item/Item';
 import styles from './ItemList.module.css';
-import List from '@material-ui/core/List';
-import PropTypes from 'prop-types';
 
+const ItemList = ({ items, onClickDone, onClickDelete, filterItems }) => (
+  <ul className={styles.list}>
+    { filterItems().map((item) => <li className={styles.li} key={item.value}>
+    <Item
+      value={item.value}
+      isDone={item.isDone}
+      id={item.id}
+      onClickDone={onClickDone}
+      onClickDelete={onClickDelete}
+    />
+  </li>)}
+</ul>);
 
-const ItemList = ({items, onClickDone, onClickDelete, onClickAdd}) => (<List className={styles.list}>
-  {items.map(item => (
-                    <li key={item.value}><Item value={item.value} isDone={item.isDone} id={item.id} onClickDone={onClickDone} onClickDelete={onClickDelete} onClickAdd={onClickAdd}/></li>))}
-  </List>
-  );
-ItemList.propTypes = {
-  isDone:  PropTypes.bool,
-  value: PropTypes.string.isRequired,
-  onClickDone: PropTypes.func.isRequired,
-  onClickDelete: PropTypes.func.isRequired,
-  onClickAdd: PropTypes.func.isRequired,
-  id: PropTypes.number.isRequired
-};
 export default ItemList;
