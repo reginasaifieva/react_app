bdc8ce756e0bcf79fa2ce82fb6f9fe39576c4e54
reginasaifieva <regina.saifieva98.com>
2021-06-21 16:22:12 +0300

    Updates

---
+++ precache-manifest.304f20c4899164a82407ad2dfb49a7f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731b3dc3f318c881ed9531de609ed8e1",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "bf3a5c52a6afced4a048",
+    "url": "/react_app/static/css/main.68eb6ffa.chunk.css"
+  },
+  {
+    "revision": "3dad5b0bd615788c0721",
+    "url": "/react_app/static/js/2.70043ee6.chunk.js"
+  },
+  {
+    "revision": "9230f7c4dbd642e80c48e1240613b9a9",
+    "url": "/react_app/static/js/2.70043ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3a5c52a6afced4a048",
+    "url": "/react_app/static/js/main.7c6d1bbc.chunk.js"
+  },
+  {
+    "revision": "b923d741eeba4bf347a1",
+    "url": "/react_app/static/js/runtime-main.80b4d2f4.js"
+  },
+  {
+    "revision": "141d0857b4207206ab40f656ffcd8fb7",
+    "url": "/react_app/static/media/background.141d0857.jpg"
+  }
+]);
